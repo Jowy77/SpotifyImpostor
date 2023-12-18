478c901f7bd5ccf0068c4f9501e2d63b6c2ff89f
Jowy77 <Iproyectz@hotmail.com>
2023-12-18 19:58:06 +0000

    18-12-2023: Creacion de la interfaz y el model

---
+++ src/main/java/com/example/spotifyimpostor/modeloPantallas/MediaPlayerViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.spotifyimpostor.modeloPantallas
+
+class MediaPlayerViewModel {
+}
---
+++ src/main/java/com/example/spotifyimpostor/pantallas/MediaPlayerView.kt
@@ -0,0 +1,11 @@
+package com.example.spotifyimpostor.pantallas
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MediaPlayerView() {
+    Column {
+
+    }
+}
